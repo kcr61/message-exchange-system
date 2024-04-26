c05b5e3d58d8927942998f625a8daa3f6e8ec042
kchelle robinson <rkchelle64@gmail.com>
2024-04-26 13:04:22 -0500

    modified:   app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const cookieParser = require('cookie-parser');
+const userController = require('./controllers/userController');
 const User = require('./models/user');
 const Topic = require('./models/topic');
 const Message = require('./models/message');

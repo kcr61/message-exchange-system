495751220a4dcb0aee66d7c489ba27789a0e2405
kcr61 <81253213+kcr61@users.noreply.github.com>
2024-05-01 21:20:25 -0500

    Delete routes/messageRoutes.js

--- routes/messageRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const messageController = require('../controllers/messageController');
-
-// Message posting route
-router.post('/post', messageController.postMessage);
-
-module.exports = router;
